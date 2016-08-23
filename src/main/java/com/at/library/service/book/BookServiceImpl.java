77ee3c431435de3b78f39bfbab32e61cc698ed48
Lucybtf <lucybtf@gmail.com>
2016-08-23 20:17:38 +0200

    Curso Día 2. Book Service es la interfaz de nuestro Servicio y BookServiceImpl será donde implementemos nuestro servicio para que el controlador haga uso de dicha interfaz. Estos dos servicios contendrán los tranformadores entre la clase Book y el BookDTO y viceversa.

--- src/main/java/com/at/library/service/book/BookServiceImpl.java
+++ src/main/java/com/at/library/service/book/BookServiceImpl.java
@@ -1,7 +1,7 @@
-package com.at.library.service;
+package com.at.library.service.book;
+
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -10,28 +10,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.at.library.dao.BookDao;
-import com.at.library.dto.*;
+import com.at.library.dto.BookDTO;
 import com.at.library.model.Book;
 
 @Service
-public class BookServiceImpl implements BookService{
+public class BookServiceImpl implements BookService {
 
 	@Autowired
 	private BookDao bookDao;
-	
-	private DozerBeanMapper dozer= new DozerBeanMapper();
-	
-	public List<BookDTO> getAll() {
-		final Iterable<Book> findAll=bookDao.findAll();
-		final Iterator<Book> iterator=findAll.iterator();
+
+	@Autowired
+	private DozerBeanMapper dozer;
+
+	@Override
+	public List<BookDTO> findAll() {
+		final Iterable<Book> findAll = bookDao.findAll();
+		final Iterator<Book> iterator = findAll.iterator();
 		final List<BookDTO> res = new ArrayList<>();
-		while (iterator.hasNext()){
-			final Book b= iterator.next();
-			final BookDTO bDTO=dozer.map(b, BookDTO.class)
+		while (iterator.hasNext()) {
+			final Book b = iterator.next();
+			final BookDTO bDTO = transform(b);
 			res.add(bDTO);
 		}
-		/*final BookDTO l1 = new BookDTO("111aaa", "El serñor de los anillos", "El tokien");
-		final BookDTO l2 = new BookDTO("111bbb", "La comunidad del anillo", "Tolkien");
-		return Arrays.asList(l1, l2);*/
+		return res;
 	}
+
+	@Override
+	public BookDTO transform(Book book) {
+		return dozer.map(book, BookDTO.class);
+	}
+
+	@Override
+	public Book transform(BookDTO book) {
+		return dozer.map(book, Book.class);
+	}
+
 }
