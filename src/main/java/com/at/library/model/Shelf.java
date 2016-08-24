d3bb91f35e363481b8c89abf350eabb8b0f91985
Lucybtf <lucybtf@gmail.com>
2016-08-24 20:36:23 +0200

    Actualización de las relaciones entre las clases.

--- src/main/java/com/at/library/model/Shelf.java
+++ src/main/java/com/at/library/model/Shelf.java
@@ -2,9 +2,12 @@ package com.at.library.model;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 @Entity
@@ -17,7 +20,7 @@ public class Shelf {
 	private String codeshelf;
 	
 	
-	@OneToMany
+	 @OneToMany(fetch=FetchType.LAZY, cascade = CascadeType.ALL)
 	private List<Book> books;
 
 
