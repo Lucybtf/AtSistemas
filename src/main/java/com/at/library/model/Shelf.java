dedb684786824f796db7a05d759dfac2aa2c49fd
Lucybtf <lucybtf@gmail.com>
2016-08-24 21:12:29 +0200

    Añadimos el tipo cascade, para que se actualicen los libros, cuando se ejecute una operación de alta o baja de libros.

--- src/main/java/com/at/library/model/Shelf.java
+++ src/main/java/com/at/library/model/Shelf.java
@@ -2,12 +2,9 @@ package com.at.library.model;
 
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 @Entity
@@ -20,7 +17,7 @@ public class Shelf {
 	private String codeshelf;
 	
 	
-	 @OneToMany(fetch=FetchType.LAZY, cascade = CascadeType.ALL)
+	@OneToMany
 	private List<Book> books;
 
 
