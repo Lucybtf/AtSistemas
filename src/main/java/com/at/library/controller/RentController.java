939267a4b0abac7c08f21ec923b2ba11ec5953bb
Lucybtf <lucybtf@gmail.com>
2016-08-26 18:37:07 +0200

    Actualizacióm del Class RentController con las anotaciones @RestController y @RequestMapping

--- src/main/java/com/at/library/controller/RentController.java
+++ src/main/java/com/at/library/controller/RentController.java
@@ -1,5 +1,10 @@
 package com.at.library.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/rent")
 public class RentController {
 
 }
