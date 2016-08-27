363f240ff33164fec9b14c77dad82bde8574dd17
Lucybtf <lucybtf@gmail.com>
2016-08-28 01:54:22 +0200

    Comento las funciones de rentBook y returnBook.

--- src/main/java/com/at/library/controller/RentController.java
+++ src/main/java/com/at/library/controller/RentController.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.at.library.dto.RentDTO;
-import com.at.library.service.book.RentService;
+import com.at.library.dto.UserDTO;
+import com.at.library.service.rent.RentService;
 
 @RestController
 @RequestMapping(value = "/rent")
@@ -18,7 +19,7 @@ public class RentController {
 
 	private static final Logger log = LoggerFactory.getLogger(RentController.class);
 	
-	@Autowired
+	//LA LOGICA VA EN EL SERVICIO
 	private RentService rentService;
 	
 	@RequestMapping( method = { RequestMethod.GET})
@@ -32,5 +33,21 @@ public class RentController {
 		log.debug(String.format("Eliminar el Alquiler con el id: %s", idrent));
 		rentService.delete(idrent);
 	}
+	
+	@RequestMapping( value="/{id}", method = { RequestMethod.GET})
+	public RentDTO findOne(@PathVariable("id")Integer id){
+		log.debug(String.format("Buscando el Rent con el id %s", id));
+		return rentService.findbyId(id);
+	}
+/*	@RequestMapping( value="/{idbook}", method = { RequestMethod.PUT })
+	public void rentBook(@PathVariable("idbook")Integer id){
+		rentService.rentBook(id);
+	}
+	
+
+	@RequestMapping( value="/disable/{id}", method = { RequestMethod.PUT })
+	public void returnBook(@PathVariable("id")Integer id){
+		rentService.returnBook(id);
+	}*/
 	
 }
