6b93019506d948e86088685ee8146fce789f17fb
Lucybtf <lucybtf@gmail.com>
2016-08-27 20:11:52 +0200

    Actualización de errores del Controlador de Usuario, ya funcionando.

--- src/main/java/com/at/library/controller/UserController.java
+++ src/main/java/com/at/library/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,7 @@ import com.at.library.dto.BookDTO;
 import com.at.library.dto.UserDTO;
 import com.at.library.enums.StatusEnum;
 import com.at.library.model.User;
-import com.at.library.service.book.UserService;
+import com.at.library.service.user.UserService;
 
 @RestController
 @RequestMapping(value = "/user")
@@ -22,25 +23,39 @@ public class UserController {
 
 	private static final Logger log = LoggerFactory.getLogger(UserController.class);
 	
+	@Autowired
 	private UserService userService;
 	
+	/*FUNCIONA PERFECTAMENTE*/
+	@RequestMapping(value="/{id}" , method = {RequestMethod.GET})
+	public UserDTO findOne(@PathVariable("id")Integer id){
+		log.debug(String.format("Buscando el Usuario con el id %s", id));
+		log.debug(String.format("Buscando:", userService.findbyId(id)));
+		return userService.findbyId(id);
+	}
+	
+	/*FUNCIONA PERFECTAMENTE*/
 	@RequestMapping( method = { RequestMethod.POST })
 	public UserDTO create(@RequestBody UserDTO user) {
+		log.debug(String.format("Vamos a crear el libro siguiente", user));
 		return userService.create(user);
 	}
 	
-	@RequestMapping( method = { RequestMethod.DELETE })
-	public void delete(@RequestBody Integer user) {
+	/*FUNCIONA PERFECTAMENTE*/
+	@RequestMapping(value="/{id}", method = { RequestMethod.DELETE })
+	public void delete(@PathVariable("id") Integer user) {
+		log.debug("Borrar un usuario  del sistema dado su id: %s", user);
 		userService.delete(user);
 	}
 	
-	
-	@RequestMapping( method = { RequestMethod.PUT })
+	/*FUNCIONA PERFECTAMENTE: pero no se pinta en el JSON*/
+	@RequestMapping( value="/active/{id}", method = { RequestMethod.PUT })
 	public void activeUser(@PathVariable("id")Integer id){
 		userService.activeUser(id);
 	}
 	
-	@RequestMapping( method = { RequestMethod.PUT })
+	/*FUNCIONA PERFECTAMENTE: pero no se pinta en el JSON*/
+	@RequestMapping( value="/disable/{id}", method = { RequestMethod.PUT })
 	public void disableUser(@PathVariable("id")Integer id){
 		userService.disableUser(id);
 	}
