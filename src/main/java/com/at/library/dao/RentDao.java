99b3cc24c176110dab2051b311e1feddd8ec89dc
Lucybtf <lucybtf@gmail.com>
2016-08-28 17:35:38 +0200

    Modificación del Dao de Rent

--- src/main/java/com/at/library/dao/RentDao.java
+++ src/main/java/com/at/library/dao/RentDao.java
@@ -1,13 +1,10 @@
 package com.at.library.dao;
 
-import java.util.Date;
-import java.util.List;
+
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.at.library.dto.BookDTO;
-import com.at.library.dto.RentDTO;
-import com.at.library.model.Book;
+
 import com.at.library.model.Rent;
 
 public interface RentDao extends CrudRepository<Rent, Integer>{
