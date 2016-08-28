4b046babb2b9191ff1919f4f9e94df7c0103507c
Lucybtf <lucybtf@gmail.com>
2016-08-28 12:11:11 +0200

    Comento la declaración de findByBookAndDate NO ME FUNCIONA.

--- src/main/java/com/at/library/dao/RentDao.java
+++ src/main/java/com/at/library/dao/RentDao.java
@@ -1,10 +1,17 @@
 package com.at.library.dao;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
+import com.at.library.dto.BookDTO;
+import com.at.library.dto.RentDTO;
 import com.at.library.model.Book;
 import com.at.library.model.Rent;
 
 public interface RentDao extends CrudRepository<Rent, Integer>{
 
+	//List<RentDTO> findByBook(BookDTO b);
+	 //Rent findByBookAndDate(BookDTO book, Date date);
 }
