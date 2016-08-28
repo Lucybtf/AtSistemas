06263c58059e2e159d7203ca411cf9c4693bb4a6
Lucybtf <lucybtf@gmail.com>
2016-08-29 00:24:55 +0200

    Añado la Query que me devuelve un RentDTO por idUser, idBook y idEmployee.

--- src/main/java/com/at/library/dao/RentDao.java
+++ src/main/java/com/at/library/dao/RentDao.java
@@ -2,13 +2,15 @@ package com.at.library.dao;
 
 
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-
+import com.at.library.dto.RentDTO;
 import com.at.library.model.Rent;
 
 public interface RentDao extends CrudRepository<Rent, Integer>{
 
-	//List<RentDTO> findByBook(BookDTO b);
-	 //Rent findByBookAndDate(BookDTO book, Date date);
+	@Query("select new com.at.library.dto.RentDTO(r.rentpk.book.id, r.user.id, r.employee.id) from Rent as r where  r.rentpk.book.id=?1")
+	RentDTO findByUserAndBook(Integer idLibro);
+
 }
