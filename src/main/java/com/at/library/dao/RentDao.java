c37697d8c723cfaaf0bc2c4dfa2f0be4d791e9b6
Lucybtf <lucybtf@gmail.com>
2016-08-29 18:58:10 +0200

    Modificación de los Dao, añado las etiquetas de @Repository olvidadas.

--- src/main/java/com/at/library/dao/RentDao.java
+++ src/main/java/com/at/library/dao/RentDao.java
@@ -4,10 +4,12 @@ package com.at.library.dao;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.at.library.dto.RentDTO;
 import com.at.library.model.Rent;
 
+@Repository
 public interface RentDao extends CrudRepository<Rent, Integer>{
 
 	@Query("select new com.at.library.dto.RentDTO(r.rentpk.book.id, r.user.id, r.employee.id, r.rentpk.initDate) from Rent as r where  r.rentpk.book.id=?1")
