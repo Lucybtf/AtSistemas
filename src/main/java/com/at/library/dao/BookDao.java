3d351f400c9049374280ad885cb5564f5c30e890
Lucybtf <lucybtf@gmail.com>
2016-08-29 22:27:27 +0200

    Corregimos la Query para que nos devuelva la lista de BookDTOs.

--- src/main/java/com/at/library/dao/BookDao.java
+++ src/main/java/com/at/library/dao/BookDao.java
@@ -24,8 +24,8 @@ public interface BookDao extends CrudRepository<Book, Integer> {
 	Book findByAuthor(String author);
 	
 	
-	@Query("select r.rentpk.book.id from Rent as r where (r.endDate is null and r.rentpk.book.id=?1)")
-	List<Book> findBooksAvailable();
+	@Query("select new com.at.library.dto.BookDTO(b.id, b.isbn, b.title, b.author) from Book as b where b.id in (select r.rentpk.book.id from Rent as r where r.endDate is not null)")
+	List<BookDTO> findBooksAvailable();
 	
 	//@Query(value = "SELECT r.pk.book.id FROM Rent AS r, Book AS b WHERE (r.endDate IS null AND r.pk.book.id = ?1) OR (b.id = ?1 AND b.status = 'DISABLE')")
 	@Query("select r.rentpk.book.id from Rent as r where (r.endDate is null and r.rentpk.book.id=?1)")
