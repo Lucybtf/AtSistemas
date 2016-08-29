277c28ba67cdd991eb04ebe489856105c1a46305
Lucybtf <lucybtf@gmail.com>
2016-08-29 21:18:37 +0200

    Añadimos las busquedas por title, isbn, author usando Spring.

--- src/main/java/com/at/library/dao/BookDao.java
+++ src/main/java/com/at/library/dao/BookDao.java
@@ -13,8 +13,22 @@ import com.at.library.model.Book;
 @Repository
 public interface BookDao extends CrudRepository<Book, Integer> {
 
+	/** Busqueda de los libros:
+	 *  - title
+	 *  - isbn
+	 *  - author
+	 *  - disponibilidad
+	 * */
+	Book findByTitle(String title);
+	Book findByIsbn(String isbn);
+	Book findByAuthor(String author);
+	
+	
+	@Query("select r.rentpk.book.id from Rent as r where (r.endDate is null and r.rentpk.book.id=?1)")
+	List<Book> findBooksAvailable();
+	
 	//@Query(value = "SELECT r.pk.book.id FROM Rent AS r, Book AS b WHERE (r.endDate IS null AND r.pk.book.id = ?1) OR (b.id = ?1 AND b.status = 'DISABLE')")
 	@Query("select r.rentpk.book.id from Rent as r where (r.endDate is null and r.rentpk.book.id=?1)")
 	//@Query("SELECT new com.at.library.dto.BookDTO(b.id, b.isbn, b.title, b.author) from Book as b where b.id in (select r.rentpk.book.id from Rent as r where r.endDate is null)")
-	Integer findunAvailable(Integer id);
+	Integer checkAvailability(Integer id);
 }
