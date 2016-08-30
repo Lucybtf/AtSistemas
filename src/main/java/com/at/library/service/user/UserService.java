b0622d7187a2fa6391b0898f1f9ce11430f67a0a
Lucybtf <lucybtf@gmail.com>
2016-08-30 21:49:07 +0200

    Añado UserNotFoundException en la interfaz de UserService.

--- src/main/java/com/at/library/service/user/UserService.java
+++ src/main/java/com/at/library/service/user/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.at.library.dto.BookDTO;
 import com.at.library.dto.UserDTO;
+import com.at.library.exceptions.UserNotFoundException;
 import com.at.library.model.User;
 
 public interface UserService {
@@ -30,18 +31,21 @@ public interface UserService {
 	
 	/**
 	 * Encontrar un usuario por Id
+	 * @throws UserNotFoundException 
 	 * */
-	UserDTO findbyId(Integer id);
+	UserDTO findbyId(Integer id) throws UserNotFoundException;
 
 	/**
 	 * Activar un usuario
+	 * @throws UserNotFoundException 
 	 * **/
-	void activeUser(Integer id);
+	void activeUser(Integer id) throws UserNotFoundException;
 	
 	/**
 	 * Desactivar un usuario
+	 * @throws UserNotFoundException 
 	 * **/
-	void disableUser(Integer id);
+	void disableUser(Integer id) throws UserNotFoundException;
 
 	/**
 	 * Buscar el Usuario por Dni
