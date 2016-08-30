bbf527af40fa3c03360ab5bdac11f97e8352f9b1
Lucybtf <lucybtf@gmail.com>
2016-08-30 19:16:57 +0200

    Borro la Query comentada

--- src/main/java/com/at/library/dao/BookDao.java
+++ src/main/java/com/at/library/dao/BookDao.java
@@ -29,6 +29,5 @@ public interface BookDao extends CrudRepository<Book, Integer> {
 	
 	//@Query(value = "SELECT r.pk.book.id FROM Rent AS r, Book AS b WHERE (r.endDate IS null AND r.pk.book.id = ?1) OR (b.id = ?1 AND b.status = 'DISABLE')")
 	@Query("select r.rentpk.book.id from Rent as r where (r.endDate is null and r.rentpk.book.id=?1)")
-	//@Query("SELECT new com.at.library.dto.BookDTO(b.id, b.isbn, b.title, b.author) from Book as b where b.id in (select r.rentpk.book.id from Rent as r where r.endDate is null)")
 	Integer checkAvailability(Integer id);
 }
