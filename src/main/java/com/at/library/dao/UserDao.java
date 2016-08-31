54c8d5b1a4f472156f1a3b208a52db2753d7406a
Lucybtf <lucybtf@gmail.com>
2016-08-31 20:42:32 +0200

    Añado las busquedas por Nombre.

--- src/main/java/com/at/library/dao/UserDao.java
+++ src/main/java/com/at/library/dao/UserDao.java
@@ -9,5 +9,7 @@ import com.at.library.model.User;
 @Repository
 public interface UserDao extends CrudRepository<User, Integer> {
 	
+	/* Busquedas por dni y por nombre en el Usuario*/
 	User findByDni(String dni);
+	User findByName(String name);
 }
