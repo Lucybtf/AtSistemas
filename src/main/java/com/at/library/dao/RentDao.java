af12d2bb51ba7685ba33368b527f2c3a83995de9
Lucybtf <lucybtf@gmail.com>
2016-08-31 12:39:42 +0200

    Modificación del RentDao

--- src/main/java/com/at/library/dao/RentDao.java
+++ src/main/java/com/at/library/dao/RentDao.java
@@ -2,6 +2,8 @@ package com.at.library.dao;
 
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -12,7 +14,11 @@ import com.at.library.model.Rent;
 @Repository
 public interface RentDao extends CrudRepository<Rent, Integer>{
 
+	//@Query("select new com.at.library.dto.RentDTO(r.rentpk.book.id, r.user.id, r.employee.id, r.rentpk.initDate) from Rent as r where  (r.rentpk.book.id=?1 and r.endDate is null)")
+	//RentDTO findByUserAndBook(Integer idLibro);
+	
+	//Devuelve el "unico" Libro alquilado correspondiente a un id
 	@Query("select new com.at.library.dto.RentDTO(r.rentpk.book.id, r.user.id, r.employee.id, r.rentpk.initDate) from Rent as r where  r.rentpk.book.id=?1")
-	RentDTO findByUserAndBook(Integer idLibro);
+	List<RentDTO> findByBook(Integer idbook);
 
 }
