9959a88129e590ac6895c518da92a96179a3cbf2
Lucybtf <lucybtf@gmail.com>
2016-09-03 21:51:37 +0200

    Modificamos la Query de Historial de Alquileres usando el DTO HistoryRentedDTO y modificamos query de findByBook

--- src/main/java/com/at/library/dao/RentDao.java
+++ src/main/java/com/at/library/dao/RentDao.java
@@ -8,7 +8,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.at.library.dto.HistoryRentedDTO;
 import com.at.library.dto.RentDTO;
+import com.at.library.model.Book;
 import com.at.library.model.Rent;
 
 @Repository
@@ -16,9 +18,14 @@ public interface RentDao extends CrudRepository<Rent, Integer>{
 
 	//@Query("select new com.at.library.dto.RentDTO(r.rentpk.book.id, r.user.id, r.employee.id, r.rentpk.initDate) from Rent as r where  (r.rentpk.book.id=?1 and r.endDate is null)")
 	//RentDTO findByUserAndBook(Integer idLibro);
+	//@Query("select r from Rent as r  where r.rentpk.book.status='ACTIVE'")
+	//List<Rent> findAll();
 	
 	//Devuelve el "unico" Libro alquilado correspondiente a un id
-	@Query("select new com.at.library.dto.RentDTO(r.rentpk.book.id, r.user.id, r.employee.id, r.rentpk.initDate) from Rent as r where  r.rentpk.book.id=?1")
-	List<RentDTO> findByBook(Integer idbook);
+	@Query("select r from Rent as r where  r.rentpk.book.id=?1 and r.endDate is null")
+	Rent findByBook(Integer idbook);
+
+	@Query("select new com.at.library.dto.HistoryRentedDTO(r.rentpk.initDate, r.endDate, r.rentpk.book.title, r.rentpk.book.id) from Rent as r where r.rentpk.book.status = 'ACTIVE'")
+	List<HistoryRentedDTO> RentsHistory();
 
 }
