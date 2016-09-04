0f93534a9dea8df765f05270a5aa5870a632e1b6
Lucybtf <lucybtf@gmail.com>
2016-09-04 21:13:33 +0200

    Añadimos el autor a la aplicación.

--- src/main/java/com/at/library/LibraryApplication.java
+++ src/main/java/com/at/library/LibraryApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Lucía Batista Flores
+ *
+ */
 @SpringBootApplication
 @EnableAutoConfiguration
 public class LibraryApplication {
