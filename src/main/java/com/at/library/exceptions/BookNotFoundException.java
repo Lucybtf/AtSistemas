e9d405648cea097c0627c18982032c4275b566be
Lucybtf <lucybtf@gmail.com>
2016-09-04 15:37:31 +0200

    Elimino las anotaciones @ResponseStatus de las Excepciones, las colocamos en el controlador.

--- src/main/java/com/at/library/exceptions/BookNotFoundException.java
+++ src/main/java/com/at/library/exceptions/BookNotFoundException.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 
-@ResponseStatus(value=HttpStatus.NOT_FOUND)
+//@ResponseStatus(value=HttpStatus.NOT_FOUND)
 public class BookNotFoundException extends Exception {
 
 	private static final long serialVersionUID = 1644583033208122457L;
--- src/main/java/com/at/library/exceptions/EmployeeNotFoundException.java
+++ src/main/java/com/at/library/exceptions/EmployeeNotFoundException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseBody
-@ResponseStatus(value=HttpStatus.NOT_FOUND)
+//@ResponseBody
+//@ResponseStatus(value=HttpStatus.NOT_FOUND)
 public class EmployeeNotFoundException extends Exception {
 
 
