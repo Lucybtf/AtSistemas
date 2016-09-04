9096e36f796b2556d4dd449a7c427ded761bfd15
Lucybtf <lucybtf@gmail.com>
2016-09-04 15:34:07 +0200

    Añadimos una Tabla de Usuarios Castigados al Modelo con: -id -user -startDate -endDate

---
+++ src/main/java/com/at/library/model/PunishmentUser.java
@@ -0,0 +1,68 @@
+package com.at.library.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class PunishmentUser implements Serializable {
+
+
+	private static final long serialVersionUID = -8510481102080598208L;
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	@Temporal(TemporalType.DATE)
+	private Date startDate;
+	
+	@Temporal(TemporalType.DATE)
+	private Date endDate;
+	
+	private Integer punishmentdays;
+	
+	@ManyToOne
+	private User user;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+
+}
